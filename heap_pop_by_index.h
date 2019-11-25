108cf8ad652a6bf32dcf7bdb6da306bfede7e388
YuuKiLeeNa <39462742+YuuKiLeeNa@users.noreply.github.com>
2019-11-25 22:32:37 +0800

    Update heap_pop_by_index.h

--- heap_pop_by_index.h
+++ heap_pop_by_index.h
@@ -1,16 +1,18 @@
 
 #ifndef __HEAP_OPERATION_H__
 #define __HEAP_OPERATION_H__
+
 #include<functional>
 
-
-template<typename ITER, typename DIFF, typename PR>
-void Pop_heap_to_bottom_by_index(ITER iterBeg, DIFF endOffSet, DIFF hole, PR& pr)
-{
 #define GET_LEFT_CHILD_INDEX(index) (((index+1)<<1) - 1)
 #define GET_RIGHT_CHILD_INDEX(index) ((index+1)<<1)
 #define GET_PARENT_INDEX(index)	(((index+1)>>1) - 1)
 
+
+
+template<typename ITER, typename DIFF, typename PR>
+void Heap_pop_to_bottom_by_index(ITER iterBeg, DIFF endOffSet, DIFF hole, PR& pr)
+{
 	if (endOffSet < 2 || hole == endOffSet - 1)
 		return;
 
@@ -41,16 +43,50 @@ void Pop_heap_to_bottom_by_index(ITER iterBeg, DIFF endOffSet, DIFF hole, PR& pr
 			}
 			if (pr(holeValue, *(iterBeg + destIndex)))
 			{
-				*(iterBeg + hole) = *(iterBeg + destIndex);
+				*(iterBeg + hole) = std::move(*(iterBeg + destIndex));
 				hole = destIndex;
 			}
 			else break;
 		}
 	}
 	*(iterBeg + hole) = std::move(holeValue);
-#undef GET_LEFT_CHILD_INDEX
-#undef GET_RIGHT_CHILD_INDEX
-#undef GET_PARENT_INDEX
+}
+
+template<typename ITER, typename DIFF, typename PREDICATE, typename CHANGEVALUE_TYPE, typename OPERATION>
+void Heap_change_index_value(ITER begIter, DIFF endOffSet, DIFF hole, PREDICATE &pr, const CHANGEVALUE_TYPE &v, OPERATION &op)
+{
+
+	typename std::remove_reference<decltype(*begIter)>::type holeValue = op(std::move(*(begIter + hole)), v);
+	if (hole > 0 && pr(*(begIter + GET_PARENT_INDEX(hole)), holeValue)) 
+	{
+		do 
+		{
+			auto parentIndex = GET_PARENT_INDEX(hole);
+			*(begIter + hole) = std::move(*(begIter + parentIndex));
+			hole = parentIndex;
+		} while (hole > 0 && pr(*(begIter + GET_PARENT_INDEX(hole)), holeValue));
+	}
+	else
+	{
+		while (GET_LEFT_CHILD_INDEX(hole) < endOffSet)
+		{
+			auto left = GET_LEFT_CHILD_INDEX(hole);
+			auto right = GET_RIGHT_CHILD_INDEX(hole);
+			auto dest = left;
+			if (right < endOffSet) 
+			{
+				if (pr(*(begIter + left), *(begIter + right)))
+					dest = right;
+			}
+			if (pr(holeValue, *(begIter + dest)))
+			{
+				*(begIter + hole) = std::move(*(begIter + dest));
+				hole = dest;
+			}
+			else break;
+		}
+	}
+	*(begIter + hole) = std::move(holeValue);
 }
 
 #endif
